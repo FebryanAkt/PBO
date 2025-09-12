b919dd2a979e2cabef10134038141bc2ad0c4a1a
FebryanAkt <febryan08022006@gmail.com>
2025-09-12 13:09:39 +0700

    pertemuan 2 teori

---
+++ pertemuan2/Nasabah.java
@@ -0,0 +1,11 @@
+package pertemuan2;
+
+public class Nasabah {
+
+    public String nama;
+    public int saldo;
+
+    public void tampilkanSaldo(String mataUang){
+        System.out.println("Saldo " + nama + " adalah " + mataUang + saldo);
+    }
+}
