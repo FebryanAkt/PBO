8fb250682fd9c3ddd40def522c25ebe485f5a474
FebryanAkt <febryan08022006@gmail.com>
2025-10-19 16:58:25 +0700

    UTS

---
+++ UTS/Robot.java
@@ -0,0 +1,12 @@
+public class Robot {
+    public String name;
+    public int CPUSpeed;
+    public int power;
+
+    public void increasePower(){
+        power += 10;
+    }
+    public void reducePower(){
+        power -= 10;
+    }
+}
