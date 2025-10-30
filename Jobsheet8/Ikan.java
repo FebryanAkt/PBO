72d405111d2a68962edf59f7069d17694b6f0f0e
FebryanAkt <febryan08022006@gmail.com>
2025-10-30 10:17:47 +0700

    percobaan

---
+++ Jobsheet8/Ikan.java
@@ -0,0 +1,8 @@
+public class Ikan extends Hewan {
+    
+    @Override
+    public void bergerak(){
+        // throw new UnsupportedOperationException("Not supported yet.");
+        System.out.println("Berenang dengan SIRIP, \"wush..wush..\"");
+    }
+}
---
+++ Jobsheet8/Kucing.java
@@ -0,0 +1,9 @@
+public class Kucing extends Hewan{
+
+    @Override
+    public void bergerak(){
+        // throw new UnsupportedOperationException("Not supported yet.");
+        System.out.println("Berjalan dengan KAKI, \"tap..tap..\"");
+    }
+    
+}
