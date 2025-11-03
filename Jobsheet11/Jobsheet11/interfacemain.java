b955e57e476b6d972074f920ae3200fd92e8d1ee
FebryanAkt <febryan08022006@gmail.com>
2025-11-03 09:41:28 +0700

    percobaan 1

--- Jobsheet11/Jobsheet11/interfacemain.java
+++ Jobsheet11/Jobsheet11/interfacemain.java
@@ -3,14 +3,11 @@
         public static void main(String[] args) {
             Rektor pakrektor = new Rektor();
 
-            // Mahasiswa mhsBiasa = new Mahasiswa("Charlie");
             Sarjana sarjanaCumlaude = new Sarjana("Dini");
             PascaSarjana masterCumlaude = new PascaSarjana("Elok");
 
-            // pakrektor.beriSertifikatCumlaude(mhsBiasa);
             pakrektor.beriSertifikatCumlaude(sarjanaCumlaude);
             pakrektor.beriSertifikatCumlaude(masterCumlaude);
 
         }
     }
-
