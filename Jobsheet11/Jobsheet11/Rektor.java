db3ed24568d8de269ceec415e948c7874fb96271
FebryanAkt <febryan08022006@gmail.com>
2025-11-04 08:08:57 +0700

    pertanyaan percobaan 1

--- Jobsheet11/Jobsheet11/Rektor.java
+++ Jobsheet11/Jobsheet11/Rektor.java
@@ -2,7 +2,9 @@ public class Rektor {
     public void beriSertifikatCumlaude(ICumlaude mahasiswa){
         System.out.println("Saya REKTOR, memberikan sertifikat cumlaude");
         System.out.println("Selamat! silahkan perkenalkan diri Anda..");
-
+        
+        ((Mahasiswa)mahasiswa).kuliahDiKampus();
+        
         mahasiswa.Lulus();
         mahasiswa.meraihIPKTinggi();
 
